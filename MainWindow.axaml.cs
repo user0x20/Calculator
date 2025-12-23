4fe7ea699f88150efc1a766a442abcb63c1b1b23
Sanfix <pochta>
2025-12-23 19:13:32 +0300

    Можно теперь менять операцию

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -10,12 +10,17 @@ public partial class MainWindow : Window
     public void Add(object button, RoutedEventArgs e){
         String content = (String)((Button)button).Content;
         if (content == "CE"){
-            Operations.Text = "ㅤ";
+            Result.Text = "";
+            Operations.Text = "";
         } else if (content == "+" || content == "-" || content == "×" || content == "÷" || content == "%"){
-            if (Operations.Text != "" && Operations.Text[Operations.Text.Length - 1] != '\x20'){
-                Operations.Text += "\x20";
-                Operations.Text += content;
-                Operations.Text += "\x20";
+            if (Operations.Text != ""){
+                if (Operations.Text[Operations.Text.Length - 1] != '\x20'){
+                    Operations.Text += '\x20';
+                    Operations.Text += content;
+                    Operations.Text += '\x20';
+                } else {
+                    Operations.Text = Operations.Text.Substring(0, Operations.Text.Length - 2) + content + '\x20';
+                }
             }
         } else if (content == "="){
 
