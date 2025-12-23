7934efb93e47ef111d2aa724ce0d1ce9337e4030
Sanfix <pochta>
2025-12-24 00:27:49 +0300

    Update logic (need updtae)

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -9,25 +9,66 @@ namespace Calculator;
 public partial class MainWindow : Window
 {
 
-    public void Add(object button, RoutedEventArgs e){
-        String content = (String)((Button)button).Content;
-        if (content == "CE"){
-            Result.Text = "";
-            Operations.Text = "";
-        } else if (content == "+" || content == "-" || content == "×" || content == "÷" || content == "%"){
-            if (Operations.Text != ""){
-                if (Operations.Text[Operations.Text.Length - 1] != '\x20'){
-                    Operations.Text += '\x20';
-                    Operations.Text += content;
-                    Operations.Text += '\x20';
-                } else {
-                    Operations.Text = Operations.Text.Substring(0, Operations.Text.Length - 2) + content + '\x20';
-                }
+    public void AddDigit(object digit, RoutedEventArgs e){
+        String content = (String)((Button)digit).Content;
+
+        // Проверку надо дороботать
+        if(content != "." || (Operations.Text.Length != 0 && Operations.Text[Operations.Text.Length - 1] != '.' && Operations.Text[Operations.Text.Length - 1] != '\x20')){
+            Operations.Text += content;
+        }
+    }
+
+    public void Clear(object clear, RoutedEventArgs e){
+        Result.Text = "";
+        Operations.Text = "";
+    }
+
+    public void AddOper(object oper, RoutedEventArgs e){
+        String content = (String)((Button)oper).Content;
+        if (Operations.Text != "" && Operations.Text[Operations.Text.Length - 1] != '√' && Operations.Text[Operations.Text.Length - 1] != '.'){
+            if (Operations.Text[Operations.Text.Length - 1] != '\x20'){
+                Operations.Text += '\x20';
+                Operations.Text += content;
+                Operations.Text += '\x20';
+            } else {
+                Operations.Text = Operations.Text.Substring(0, Operations.Text.Length - 2) + content + '\x20';
             }
-        } else if (content == "="){
-            if (Operations.Text[Operations.Text.Length - 1] != '\x20' && Operations.Text[Operations.Text.Length - 1] != '√' && Operations.Text[Operations.Text.Length - 1] != '²' && Operations.Text[Operations.Text.Length - 1] != '.'){
+        }
+    }
+
+    public void AddPower(object power, RoutedEventArgs e){
+        if (Operations.Text != "" && Operations.Text[Operations.Text.Length - 1] != '√'&& Operations.Text[Operations.Text.Length - 1] != '²' && Operations.Text[Operations.Text.Length - 1] != '.' && Operations.Text[Operations.Text.Length - 1] != '\x20'){
+            Operations.Text += '²';
+        }
+    }
+
+    public void AddSqrt(object sqrt, RoutedEventArgs e){
+        if (Operations.Text == "" || Operations.Text[Operations.Text.Length - 1] == '\x20'){
+            Operations.Text += '√';
+        }
+    }
+
+    public void AddPoint(object point, RoutedEventArgs e){
+
+    }
+
+    public void Calculate(object calc, RoutedEventArgs e){
+            if (Operations.Text != "" && Operations.Text[Operations.Text.Length - 1] != '\x20' && Operations.Text[Operations.Text.Length - 1] != '√' && Operations.Text[Operations.Text.Length - 1] != '.'){
                 bool Error = false;
                 List<String> NumAndOper = Operations.Text.Split('\x20').ToList();
+
+                for (int i = 0; i < NumAndOper.Count; i++){
+                    if (NumAndOper[i].IndexOf('²') != -1){
+                        if (NumAndOper[i].IndexOf('√') != -1){
+                            NumAndOper[i] = NumAndOper[i].Substring(1, NumAndOper[i].Length - 1);
+                        } else {
+                            NumAndOper[i] = Math.Pow(double.Parse(NumAndOper[i].Substring(0, NumAndOper[i].Length - 1)), 2).ToString();
+                        }
+                    } else if (NumAndOper[i].IndexOf('√') != -1){
+                        NumAndOper[i] = Math.Sqrt(double.Parse(NumAndOper[i].Substring(1, NumAndOper[i].Length - 1))).ToString();
+                    }
+                }
+
                 for (int i = 0; i < NumAndOper.Count; i++){
                     if (Error) break;
                     if (NumAndOper[i] == "×" || NumAndOper[i] == "÷"){
@@ -49,6 +90,7 @@ public partial class MainWindow : Window
                         i--;
                     }
                 }
+
                 for (int i = 0; i < NumAndOper.Count; i++){
                     if (Error) break;
                     if (NumAndOper[i] == "+" || NumAndOper[i] == "-"){
@@ -66,19 +108,16 @@ public partial class MainWindow : Window
                         i--;
                     }
                 }
+
                 if (!Error){
                     Result.Text = NumAndOper[0];
                 } else {
                     Result.Text = "ERROR";
                 }
+
             } else {
                 Result.Text = "ERROR";
             }
-
-        // Проверку надо дороботать
-        } else if(content != "." || Operations.Text.Length != 0 || Operations.Text[Operations.Text.Length - 1] != '.'){
-            Operations.Text += content;
-        }
     }
 
     public MainWindow()
